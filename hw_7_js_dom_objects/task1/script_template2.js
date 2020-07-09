7a7ea0097fb0728767a2008a50458f5fd3f746c5
maked0nski <maked0nskihtc@gmail.com>
2020-07-10 01:08:43 +0300

    -- отримує елемент з класом content_1 та заміняє  в ньому тест на довільний. Текст отримати з prompt()

--- hw_7_js_dom_objects/task1/script_template2.js
+++ hw_7_js_dom_objects/task1/script_template2.js
@@ -18,13 +18,61 @@ for(i of class_linkList){
 }
 
 // -- отримує текст який зберігається в елементі з класом listElement2
+let class_listElement2 = document.getElementsByClassName('listElement2');
+for (const i of class_listElement2) {
+}
 
 // -- отримує всі елементи li та змінює ім колір фону на сірий
+let tag_li = document.getElementsByTagName('li');
+for (const i of tag_li) {
+    i.style.background = 'silver';
+}
+
 // -- отримує всі елементи 'a' та додає їм клас anchor
-// -- отримує всі елементи 'a' та у випадку, якщо текстовий контен елементу дорівнює link3, змінює йому розмір тексту на 40 пікселів
+
+let tag_a = document.getElementsByTagName('a');
+for (const i of tag_a) {
+    i.classList.add('anchor')
+}
+// -- отримує всі елементи 'a' та у випадку, якщо текстовий контен елементу дорівнює link3, 
+//    змінює йому розмір тексту на 40 пікселів
+for (const i of tag_a) {
+    if (i.textContent === 'link3'){
+        i.style.fontSize = '40px'
+    }
+}
+
+
 // -- отримує всі елементи 'a' та додає їм клас element_XXX. Де XXX - текстовий контент елементу a
+let tag_a1 = document.getElementsByTagName('a');
+for (const i of tag_a1) {
+    i.classList.add('element_'+i.textContent)
+}
+
 // -- отримує всі елементи 'sub-header' та змінює колір фону. Фон отримати з prompt()
-// -- отримує всі елементи 'sub-header' та змінює колір тексту у видаку якщо текст елемнту = content 2 segment . Колір отримати з prompt()
-// -- отримує елемент з класом content_1 та заміняє  в ньому тест на довільний. Текст отримати з prompt()
+let input_color = prompt('Введи цвет');
+let class_sub_header = document.getElementsByClassName('sub-header');
+for (const i of class_sub_header) {
+    i.style.background = input_color;
+}
+// -- отримує всі елементи 'sub-header' та змінює колір тексту у видаку якщо текст 
+//    елемнту = content 2 segment . Колір отримати з prompt()
+let class_sub_header1 = document.getElementsByClassName('sub-header');
+let input_color1 = prompt('ще раз')
+for (const i of class_sub_header1) {
+    if (i.textContent === 'content 2 segment'){
+        i.style.color = input_color1;
+    }
+}
+
+// // -- отримує елемент з класом content_1 та заміняє  в ньому тест на довільний. Текст отримати з prompt()
+// let class_sub_header2 = document.getElementsByClassName('sub-header');
+// let input_color2 = prompt('текст для линка')
+// for (const i of class_sub_header2) {
+//     if (i.textContent === 'content_1'){
+//         i.style.color = input_color2;
+//     }
+// }
+
 // -- отримати елементи p та змінити їм paddin на довільне значення
 // -- отримати елементи з класом text2 та змінити їм текст на довільне значення
