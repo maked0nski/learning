70641417f0412bba92884d832c79f01a1c64c5e8
maked0nski <maked0nskihtc@gmail.com>
2020-07-14 11:35:12 +0300

    DZ _finished

--- hw_8_functions/hw_8.js
+++ hw_8_functions/hw_8.js
@@ -366,3 +366,57 @@ function user_cities(user, citi) {
 }
 console.log(user_cities(usersWithId, citiesWithId));
 
+
+
+let rules = [
+    {
+        title: 'Первое правило Бойцовского клуба.',
+        body: 'Никому не рассказывать о Бойцовском клубе.'
+    },
+    {
+        title: 'Второе правило Бойцовского клуба.',
+        body: 'Никогда никому не рассказывать о Бойцовском клубе.'
+    },
+    {
+        title: 'Третье правило Бойцовского клуба.',
+        body: 'В схватке участвуют только двое.'
+    },
+    {
+        title: 'Четвертое правило Бойцовского клуба.',
+        body: 'Не более одного поединка за один раз.'
+    },
+    {
+        title: 'Пятое правило Бойцовского клуба.',
+        body: 'Бойцы сражаются без обуви и голые по пояс.'
+    },
+    {
+        title: 'Шестое правило Бойцовского клуба.',
+        body: 'Поединок продолжается столько, сколько потребуется.'
+    },
+    {
+        title: 'Седьмое правило Бойцовского клуба.',
+        body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
+    },
+    {
+        title: 'Восьмое и последнее правило Бойцовского клуба.',
+        body: 'Новичок обязан принять бой.'
+    },
+
+];
+function f_rules(arr) {
+    let div_1 = document.createElement('div');
+    div_1.id = 'wrap';
+    document.body.append(div_1);
+    for (let i = 0; i < arr.length; i++) {
+        let div_0 = document.createElement('div');
+        div_1.appendChild(div_0)
+        let h3 = document.createElement('h3');
+        h3.textContent = arr[i].title;
+        div_0.appendChild(h3);
+        let span = document.createElement('span');
+        span.textContent = arr[i].body;
+        div_0.appendChild(span);
+    }
+};
+
+f_rules(rules);
