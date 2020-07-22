680462c61e12870122e7570309ed1fc0e65eff9e
maked0nski <maked0nskihtc@gmail.com>
2020-07-23 01:50:35 +0200

    Hw

--- hw_11_htmlEvent/hw_11.js
+++ hw_11_htmlEvent/hw_11.js
@@ -37,9 +37,94 @@
 
 // - Создайте меню, которое раскрывается/сворачивается при клике
 
+
 // - Создать список комментариев , пример объекта коментария - {title : 'lorem', body:'lorem ipsum dolo sit ameti'}.
 // Вывести список комментариев в документ, каждый в своем блоке.
 //  Добавьте каждому комментарию по кнопке для сворачивания его body.
+
+let comment = [
+    {title: 'lorem_1', body: 'lorem ipsum dolo sit ameti'},
+    {title: 'lorem_2', body: 'lorem ipsum dolo sit ameti'},
+    {title: 'lorem_3', body: 'lorem ipsum dolo sit ameti'},
+    {title: 'lorem_4', body: 'lorem ipsum dolo sit ameti'},
+    {title: 'lorem_5', body: 'lorem ipsum dolo sit ameti'}
+    ];
+
+let block = document.createElement('div')
+document.body.appendChild(block);
+
+for (let commentKey in comment) {
+    let tag_comment = document.createElement('div');
+    let tag_title = document.createElement('h3');
+    let tag_body = document.createElement('div');
+
+    let tag_button = document.createElement('input')
+    tag_button.type = 'submit';
+    tag_button.value = 'Приховати'
+
+    block.appendChild(tag_comment);
+    tag_comment.appendChild(tag_title);
+    tag_comment.appendChild(tag_body);
+    tag_comment.appendChild(tag_button)
+
+    tag_title.innerText = comment[commentKey].title;
+    tag_body.innerText = comment[commentKey].body;
+}
+
+block.addEventListener('click', (ev) => {
+    let {target} = ev;
+    ev.preventDefault();
+    if (target.value === 'Приховати'){
+        target.previousElementSibling.style.display = 'none';
+        target.value = 'Розгорнути'
+    } else {
+        target.value = 'Приховати';
+        target.previousElementSibling.style.display = 'block';
+    }
+})
+
+// forma_comments[1].onclick = (ev) => {
+//     ev.preventDefault();
+//     if (forma_comments[1].value === 'Приховати'){
+//         coment_text_list[1].style.display = 'none';
+//         forma_comments[1].value = 'Розгорнути'
+//     } else {
+//         forma_comments[1].value = 'Приховати';
+//         coment_text_list[1].style.display = 'block';
+//     }
+// };
+// forma_comments[2].onclick = (ev) => {
+//     ev.preventDefault();
+//     if (forma_comments[2].value === 'Приховати'){
+//         coment_text_list[2].style.display = 'none';
+//         forma_comments[2].value = 'Розгорнути'
+//     } else {
+//         forma_comments[2].value = 'Приховати';
+//         coment_text_list[2].style.display = 'block';
+//     }
+// };
+// forma_comments[3].onclick = (ev) => {
+//     ev.preventDefault();
+//     if (forma_comments[3].value === 'Приховати'){
+//         coment_text_list[3].style.display = 'none';
+//         forma_comments[3].value = 'Розгорнути'
+//     } else {
+//         forma_comments[3].value = 'Приховати';
+//         coment_text_list[3].style.display = 'block';
+//     }
+// };
+// forma_comments[4].onclick = (ev) => {
+//     ev.preventDefault();
+//     if (forma_comments[4].value === 'Приховати'){
+//         coment_text_list[4].style.display = 'none';
+//         forma_comments[4].value = 'Розгорнути'
+//     } else {
+//         forma_comments[4].value = 'Приховати';
+//         coment_text_list[4].style.display = 'block';
+//     }
+// };
+//
+// console.log(document.forms);
 
 // - створити 2 форми  по 2 інпути в кожній. ствоирити кнопку при кліку на яку считується та
 // виводиться на консоль інформація з цих 2х форм.
