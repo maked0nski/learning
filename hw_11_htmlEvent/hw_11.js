e2d57e7fa64cde7fc0b7d08312288fb11b5be651
maked0nski <maked0nskihtc@gmail.com>
2020-07-26 03:07:25 +0200

    hw

--- hw_11_htmlEvent/hw_11.js
+++ hw_11_htmlEvent/hw_11.js
@@ -324,17 +324,17 @@
 // кинути алерт з попередженням.
 //     Перевірку робити при натисканні на кнопку
 
-let vocabulary = [
-    'мама', 'папа', 'вагон', 'сало',
-]
-let intp = document.createElement('input')
-document.body.appendChild(intp)
-intp.id = 'inpValue'
-
-let btnCheck = document.createElement('button')
-document.body.appendChild(btnCheck)
-btnCheck.type = 'submit'
-btnCheck.innerText = 'проверить слово'
+// let vocabulary = [
+//     'мама', 'папа', 'вагон', 'сало',
+// ]
+// let intp = document.createElement('input')
+// document.body.appendChild(intp)
+// intp.id = 'inpValue'
+//
+// let btnCheck = document.createElement('button')
+// document.body.appendChild(btnCheck)
+// btnCheck.type = 'submit'
+// btnCheck.innerText = 'проверить слово'
 
 // btnCheck.onclick = (ev) => {
 //     let values = document.getElementById("inpValue").value
@@ -389,11 +389,11 @@ function CreateMenu() {
 
     for (let i = 0; i < h1.length; i++) {
         let li = CreateLi(ul)
-        let a = CreateLink(h1, i, li)
+        CreateLink(h1, i, li)
     }
     for (let i = 0; i < h2.length; i++) {
         let li = CreateLi(ul)
-        let a = CreateLink(h2, i, li)
+        CreateLink(h2, i, li)
     }
 }
 
@@ -422,20 +422,20 @@ CreateMenu()
 
 
 
-// -- взять массив пользователей
-let usersWithAddress = [
-    {id:1,name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
-    {id:2,name: 'petya', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 1}},
-    {id:3,name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}},
-    {id:4,name: 'olya', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 90}},
-    {id:5,name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}},
-    {id:6,name: 'anya', age: 31, status: false, address: {city: 'Kyiv', street: 'Shevchenko', number: 2}},
-    {id:7,name: 'oleg', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 22}},
-    {id:8,name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 43}},
-    {id:9,name: 'masha', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 12}},
-    {id:10,name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
-    {id:11,name: 'max', age: 31, status: true, address: {city: 'Ternopil', street: 'Shevchenko', number: 121}}
-];
+// // -- взять массив пользователей
+// let usersWithAddress = [
+//     {id:1,name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+//     {id:2,name: 'petya', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 1}},
+//     {id:3,name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}},
+//     {id:4,name: 'olya', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 90}},
+//     {id:5,name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}},
+//     {id:6,name: 'anya', age: 31, status: false, address: {city: 'Kyiv', street: 'Shevchenko', number: 2}},
+//     {id:7,name: 'oleg', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 22}},
+//     {id:8,name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 43}},
+//     {id:9,name: 'masha', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 12}},
+//     {id:10,name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+//     {id:11,name: 'max', age: 31, status: true, address: {city: 'Ternopil', street: 'Shevchenko', number: 121}}
+// ];
 // Создать три чекбокса. Каждый из них активирует фильтр для вышеуказаного массива. Фильтры могут работать как вместе так и по отдельности.
 // 1й - отфильтровывает пользователей со статусом false (осталяет со статусом false)
 // 2й - оставляет старше 29 лет включительно
