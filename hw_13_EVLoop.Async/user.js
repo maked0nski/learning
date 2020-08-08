1556bed42b8a618275ec388df80f11bb7e9e9ee8
maked0nski <maked0nskihtc@gmail.com>
2020-08-08 11:41:09 +0200

    hw

--- hw_13_EVLoop.Async/user.js
+++ hw_13_EVLoop.Async/user.js
@@ -29,15 +29,22 @@ function userCard(number) {
     }
 
     function takeCredits(money) {
+        if (!transactionLimit) return console.error('Лимит изчерпан')
+        if (balance < money) return console.error('Недостаточно средств')
+        transactionLimit--
         this.balance -= money;
+        return LogHistory('Withdrawal of credits', money, dateTime())
     }
     
     function setTransactionLimit(limit) {
         transactionLimit = limit;
+        LogHistory('Transaction limit change', limit, dateTime())
     }
     
     function transferCredits(money, card) {
-        
+        let commission = money
+        takeCredits(commission += money*0.05)
+        card.putCredits(money)
     }
 
     return {
@@ -56,16 +63,39 @@ class UserAccount {
         this.cards = [];
     }
     addCard(){
-
+        if (this.cards.length > 3) return console.error('Не більше 3 карт можна мати')
+        this.cards.push(userCard(3))
     }
-
+    getCardByKey(number) {
+        return this.cards[number]
+    }
 }
 
-function dateTime() {
-    
+const dateTime = () => {
+    const options = {year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'};
+    return new Date().toLocaleDateString('en-GB', options)
 }
 
 const card3 = userCard(3); // returns an object with methods
 console.log(card3);
 card3.getCardOptions(); // returns options object with card info
 console.log(card3.getCardOptions())
+console.log(dateTime())
+card3.takeCredits(50);
+console.log(card3.getCardOptions())
+
+
+console.log('Приклад переказу коштів:---------------------------------')
+
+let user = new UserAccount('Bob');
+user.addCard()
+user.addCard()
+let card1 = user.getCardByKey(0);
+let card2 = user.getCardByKey(1);
+card1.putCredits(500);
+card1.setTransactionLimit(800);
+card1.transferCredits(300, card2);
+card2.takeCredits(50)
+
+console.log('card1',card1.getCardOptions());
+console.log('card2',card2.getCardOptions());
